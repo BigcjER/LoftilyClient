cd4905bd0bd2dc9238b9b420d312a72dfe93cac8
liuedg <liuedg@hotmail.com>
2025-06-14 19:30:32 +0800

    优化ModuleList

--- app/src/main/java/loftily/module/impl/render/ModuleList.java
+++ app/src/main/java/loftily/module/impl/render/ModuleList.java
@@ -3,6 +3,7 @@ package loftily.module.impl.render;
 import loftily.Client;
 import loftily.config.impl.DragsConfig;
 import loftily.event.impl.render.Render2DEvent;
+import loftily.event.impl.world.WorldLoadEvent;
 import loftily.gui.animation.Animation;
 import loftily.gui.animation.Easing;
 import loftily.gui.font.FontManager;
@@ -12,10 +13,13 @@ import loftily.gui.interaction.draggable.IDraggable;
 import loftily.module.Module;
 import loftily.module.ModuleCategory;
 import loftily.module.ModuleInfo;
+import loftily.utils.render.ColorUtils;
 import loftily.utils.render.RenderUtils;
 import loftily.value.impl.BooleanValue;
 import loftily.value.impl.NumberValue;
 import loftily.value.impl.mode.EasingModeValue;
+import loftily.value.impl.mode.ModeValue;
+import loftily.value.impl.mode.StringMode;
 import net.lenni0451.lambdaevents.EventHandler;
 import net.minecraft.util.text.TextFormatting;
 
@@ -23,20 +27,51 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @ModuleInfo(name = "ModuleList", category = ModuleCategory.RENDER, defaultToggled = true)
 public class ModuleList extends Module implements IDraggable {
-    
+    //不是value
     private final List<ModuleEntry> moduleEntries = new ArrayList<>();
+    private final Animation startXAnimation = new Animation(Easing.EaseOutExpo,500);
+    private Draggable draggable;
     
+    
+    //Text
     private final NumberValue fontSize = new NumberValue("FontSize", 18, 10, 30);
+    private final BooleanValue noRenderModule = new BooleanValue("NoRenderModule", true);
+    private final BooleanValue fontShadow = new BooleanValue("FontShadow", true);
+    private final BooleanValue drawTag = new BooleanValue("DrawTag", true);
+    //TextColor
+    private final ModeValue textColorMode = new ModeValue("TextColorMode", "Normal", this,
+            new StringMode("Normal"),
+            new StringMode("Rainbow"),
+            new StringMode("AnotherRainbow"),
+            new StringMode("Fade"));
+    private final NumberValue textColorRed = new NumberValue("TextColorRed", 255, 0, 255)
+            .setVisible(() -> textColorMode.is("Normal") || textColorMode.is("Fade"));
+    private final NumberValue textColorGreen = new NumberValue("TextColorRed", 255, 0, 255)
+            .setVisible(() -> textColorMode.is("Normal") || textColorMode.is("Fade"));
+    private final NumberValue textColorBlue = new NumberValue("TextColorRed", 255, 0, 255)
+            .setVisible(() -> textColorMode.is("Normal") || textColorMode.is("Fade"));
+    
+    //TagColor
+    private final ModeValue tagColorMode = new ModeValue("TagColorMode", "Normal", this,
+            new StringMode("Normal"),
+            new StringMode("Rainbow"),
+            new StringMode("AnotherRainbow"),
+            new StringMode("Fade"));
+    private final NumberValue tagColorRed = new NumberValue("TagColorRed", 130, 0, 255)
+            .setVisible(() -> tagColorMode.is("Normal") || tagColorMode.is("Fade"));
+    private final NumberValue tagColorGreen = new NumberValue("TagColorGreen", 130, 0, 255)
+            .setVisible(() -> tagColorMode.is("Normal") || tagColorMode.is("Fade"));
+    private final NumberValue tagColorBlue = new NumberValue("TagColorBlue", 130, 0, 255)
+            .setVisible(() -> tagColorMode.is("Normal") || tagColorMode.is("Fade"));
+    
     private final EasingModeValue moduleEasingMode = (EasingModeValue)
             new EasingModeValue("ModuleEasingMode", Easing.EaseOutQuad, this)
                     .setOnValueChange(mode -> moduleEntries.clear());
-    private final BooleanValue noRenderModule = new BooleanValue("NoRenderModule", true);
-    private final BooleanValue fontShadow = new BooleanValue("FontShadow", true);
-    private Draggable draggable;
     
     
     @Override
@@ -66,9 +101,7 @@ public class ModuleList extends Module implements IDraggable {
         
         List<ModuleEntry> sortedEntries = moduleEntries.stream()
                 .filter(entry -> !(entry.module.getModuleCategory() == ModuleCategory.RENDER && noRenderModule.getValue()))
-                .sorted(Comparator.comparingInt(entry -> -font.getStringWidth(
-                        entry.module.getName() + (entry.module.getTag().isEmpty() ? "" : " §f" + entry.module.getTag())
-                )))
+                .sorted(Comparator.comparingInt(entry -> -font.getStringWidth(getText(entry.module))))
                 .collect(Collectors.toList());
         
         
@@ -80,13 +113,14 @@ public class ModuleList extends Module implements IDraggable {
             totalHeight += (int) ((font.getHeight() - 2) * entry.animation.getValuef());
             
             Module module = entry.module;
-            String text = module.getName() + (module.getTag().isEmpty() ? "" : TextFormatting.GRAY + " " + module.getTag());
+            String text = getText(module);
             int stringWidth = font.getStringWidth(text);
             if (stringWidth > longestStringWidth) {
                 longestStringWidth = stringWidth;
             }
         }
         
+        //更新位置
         getDraggable().updateDrag(
                 mouse.x,
                 mouse.y,
@@ -96,36 +130,87 @@ public class ModuleList extends Module implements IDraggable {
                 event.getScaledResolution().getScaledHeight());
         
         int finalLongestStringWidth = longestStringWidth;
+        
+        AtomicInteger indexAll = new AtomicInteger();
+        AtomicInteger indexTag = new AtomicInteger();
         getDraggable().applyDragEffect(() -> {
+            startXAnimation.run(1);
             int y = getDraggable().getPosY();
             int x = getDraggable().getPosX();
+            
             for (ModuleEntry entry : sortedEntries) {
                 Module module = entry.module;
                 Animation animation = entry.animation;
                 
-                if (!module.isToggled() && animation.isFinished()) continue;
-                String text = module.getName() + (module.getTag().isEmpty() ? "" : TextFormatting.GRAY + " " + module.getTag());
-                int stringWidth = font.getStringWidth(text);
+                if (!module.isToggled() && animation.isFinished() || animation.getValuef()<= 0) continue;
+                String name = module.getName();
+                String tag = module.getTag();
                 
+                int nameWidth = font.getStringWidth(name);
+                int tagWidth = font.getStringWidth(tag);
+                int spaceWidth = font.getStringWidth(" ");
+                int fullWidth = drawTag.getValue() ? nameWidth + spaceWidth + tagWidth : nameWidth;
+                
+                //计算位置
                 boolean isLeft = event.getScaledResolution().getScaledWidth() / 2 > (x + finalLongestStringWidth / 2);
                 float baseX = getDraggable().getPosX();
                 float drawX;
                 
                 if (isLeft) {
-                    drawX = baseX;
-                    drawX -= stringWidth * (1.0f - animation.getValuef());
+                    drawX = baseX * startXAnimation.getValuef();
+                    drawX -= fullWidth * (1.0f - animation.getValuef());
                 } else {
-                    drawX = baseX + finalLongestStringWidth - stringWidth * animation.getValuef();
+                    drawX = baseX * startXAnimation.getValuef() + finalLongestStringWidth - fullWidth * animation.getValuef();
                 }
                 
+                //颜色，绘制
+                Color textColor = getTextColor(textColorMode.getValueByName(), textColorRed.getValue().intValue(), textColorGreen.getValue().intValue(), textColorBlue.getValue().intValue(), indexAll.get());
+                Color tagColor = getTextColor(tagColorMode.getValueByName(), tagColorRed.getValue().intValue(), tagColorGreen.getValue().intValue(), tagColorBlue.getValue().intValue(), indexTag.get());
                 
-                if (!fontShadow.getValue())
-                    font.drawString(text, drawX, y, -1);
-                else
-                    font.drawStringWithShadow(text, drawX, y, -1);
+                if (!fontShadow.getValue()) {
+                    font.drawString(name, drawX, y, textColor);
+                    if (drawTag.getValue()) font.drawString(tag, drawX + spaceWidth, y, tagColor);
+                } else {
+                    font.drawStringWithShadow(name, drawX, y, textColor.getRGB());
+                    if (drawTag.getValue())
+                        font.drawStringWithShadow(tag, drawX + nameWidth + spaceWidth, y, tagColor.getRGB());
+                }
+                
                 y += (int) ((font.getHeight() - 2) * animation.getValuef());
+                indexAll.getAndIncrement();
+                if (!module.getTag().isEmpty()) indexTag.getAndIncrement();
             }
         });
+    }
+    
+    @EventHandler
+    public void onWorldLoad(WorldLoadEvent event) {
+        startXAnimation.run(0);
+        startXAnimation.setValue(0);
+    }
+    
+    private Color getTextColor(String mode, int red, int green, int blue, int index) {
+        Color color = Color.WHITE;
+        switch (mode.toLowerCase()) {
+            case "normal":
+                color = new Color(red, green, blue);
+                break;
+            case "rainbow":
+                color = ColorUtils.rainbow(index + index * 200000000L);
+                break;
+            case "fade":
+                color = ColorUtils.fade(new Color(red, green, blue), index, 10);
+                break;
+            case "anotherrainbow":
+                color = ColorUtils.rainbow2(index);
+                break;
+        }
+        
+        return color;
+    }
+    
+    private String getText(Module module) {
+        return module.getName() + (drawTag.getValue() ? (module.getTag().isEmpty() ? "" : TextFormatting.GRAY + " " + module.getTag()) : "");
     }
     
     @Override
--- app/src/main/java/loftily/utils/render/ColorUtils.java
+++ app/src/main/java/loftily/utils/render/ColorUtils.java
@@ -8,4 +8,30 @@ public class ColorUtils {
     public static Color colorWithAlpha(Color color, int alpha) {
         return new Color(color.getRed(), color.getGreen(), color.getBlue(), MathHelper.clamp(alpha, 0, 255));
     }
+    
+    public static Color rainbow(long offset) {
+        float hue = (float) (System.nanoTime() + offset) / 6.0E9F % 1.0F;
+        long c = Long.parseLong(Integer.toHexString(Color.HSBtoRGB(hue, 1.0F, 1F)), 16);
+        Color color = new Color((int) c);
+        return new Color(color.getRed() / 255.0F, color.getGreen() / 255.0F, color.getBlue() / 255.0F, color.getAlpha() / 255.0F);
+    }
+    
+    public static Color rainbow2(int index) {
+        double currentColor = Math.ceil((System.currentTimeMillis() + (index * 70L)) / 7.0);
+        
+        currentColor %= 360.0;
+        
+        float hue = (float) (currentColor / 360.0);
+        
+        return Color.getHSBColor(hue, 0.5F, 1F);
+    }
+    
+    public static Color fade(Color color, int index, int count) {
+        float[] hsb = Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null);
+        float brightness = Math.abs(((System.currentTimeMillis() % 2000L) / 1000.0f
+                + (float) index / count * 2.0f) % 2.0f - 1.0f);
+        brightness = 0.5f + 0.5f * brightness;
+        hsb[2] = brightness % 2.0f;
+        return new Color(Color.HSBtoRGB(hsb[0], hsb[1], hsb[2]));
+    }
 }
