c546e5eb19125c482e42640098ab613d4486ec4d
GG_4i2 <3369888172@qq.com>
2025-06-21 00:20:43 +0800

    feat:idk

--- app/src/main/java/loftily/module/impl/movement/flys/TimerGlideFly.java
+++ app/src/main/java/loftily/module/impl/movement/flys/TimerGlideFly.java
@@ -1,5 +1,6 @@
 package loftily.module.impl.movement.flys;
 
+import loftily.event.impl.player.motion.MoveEvent;
 import loftily.event.impl.world.LivingUpdateEvent;
 import loftily.module.impl.movement.Fly;
 import loftily.utils.player.MoveUtils;
@@ -15,7 +16,7 @@ public class TimerGlideFly extends Mode<Fly> {
     }
 
     private final NumberValue timerSpeed = new NumberValue("TimerSpeed",20,10,100,0.1);
-    private final NumberValue speed = new NumberValue("Speed",0.02,0.0,0.05,0.001);
+    private final NumberValue speed = new NumberValue("Speed",0.02,0.0,0.05,0.01);
     private final BooleanValue customMotionY = new BooleanValue("CustomMotionY",false);
     private final NumberValue motionSpeed = new NumberValue("MotionSpeed",-0.01,-0.2,0.2,0.01).setVisible(customMotionY::getValue);
     private final BooleanValue smartHurt = new BooleanValue("SmartHurt",false);
@@ -48,18 +49,22 @@ public class TimerGlideFly extends Mode<Fly> {
             }
         }
 
-        if(!smartHurt.getValue() || (ticks <= 1500 && boost)) {
+        if(!smartHurt.getValue() || (ticks <= flyTicks.getValue() && boost)) {
             MoveUtils.setSpeed(speed.getValue(),false);
             mc.timer.timerSpeed = timerSpeed.getValue().floatValue();
             if(customMotionY.getValue()) {
                 mc.player.motionY = motionSpeed.getValue().floatValue();
             }else {
-                mc.player.motionY *= 0.039;
+                if(ticks <= 900){
+                    mc.player.motionY *= 0.039;
+                }else {
+                    mc.player.motionY *= 0.045;
+                }
             }
             ticks++;
 
         }
-        if(ticks > 1000 && smartHurt.getValue()){
+        if(ticks > flyTicks.getValue() && smartHurt.getValue()){
             getParent().toggle();
         }
     }
